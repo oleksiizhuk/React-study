d4cc754eec0581f2cd2994b3512bfcb8d16a8c6b
Oleksii_Zhuk <oleksiizhuk.att@gmail.com>
2021-05-10 23:07:41 +0300

    add validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,5 +1,10 @@
 import * as Yup from 'yup';
 
+// export const isEmail = (email) => /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+export const isValidName = (name) => /^(?=.{1,40}$)[a-zA-Z]+(?:[-'\s][a-zA-Z]+)*$/gm.test(name);
+export const isPhone = (phone) => /[0-9]{7}/.test(phone);
+export const isValidPassword = (value) => (/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(value));
+
 export const validationSignIn = Yup.object({
   userName: Yup.string()
     .min(4, 'Must be at least 4 characters long')
@@ -21,15 +26,41 @@ export const validationSignIn = Yup.object({
     .required('Required'),
 });
 
-export const validationSignUp = Yup.object({
-  userName: Yup.string()
-    .min(4, 'Must be at least 4 characters long')
-    .required('Required'),
-  age: Yup.number()
-    .required('Required'),
-  email: Yup.string()
-    .required('Required'),
-  password: Yup.string()
-    .matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 'Is not in correct format')
-    .required('Required'),
-});
+export const validateSignUp = (values) => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = 'isRequired';
+  } else if (!isValidName(values.firstName)) {
+    errors.firstName = 'invalidFirstName';
+  }
+
+  if (!values.lastName) {
+    errors.lastName = 'isRequired';
+  } else if (!isValidName(values.lastName)) {
+    errors.lastName = 'invalidLastName';
+  }
+
+  if (!values.phone) {
+    errors.phone = 'isRequired';
+  } else if (values.phone.length !== 7) {
+    errors.phone = 'invalidPhoneNumber';
+  } else if (!isPhone(values.phone)) {
+    errors.phone = 'invalidPhoneNumber';
+  }
+
+  if (!values.password) {
+    errors.password = 'isRequired';
+  } else if (!isValidPassword(values.password)) {
+    errors.password = 'passwordMustMatchGuidelines';
+  }
+
+  if (!values.repeatPassword) {
+    errors.repeatPassword = 'isRequired';
+  } else if (!values.password || values.password !== values.repeatPassword) {
+    errors.repeatPassword = 'passwordsDontMatch';
+  }
+
+  return errors;
+};
+
+validateSignUp();
